80d3d411635cdb31fb35b9d80a1d5b027bb5dc26
Antonio Llamas <antoniollamasln@gmail.com>
2016-08-22 10:01:18 +0200

    add LESSON 5 and LESSON 6

---
+++ basic/lesson5.flow.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Lección 5: Flujo de ejecución</title>
+</head>
+<body>
+	<?php
+
+		/* 
+
+			El flujo de ejecución de un lenguaje de programación indica el orden en el que se ejecutan las instrucciones del programa. 
+
+			En PHP el flujo de ejecución siempre va de arriba abajo 
+		
+		*/
+
+		echo "este es el primer mensaje <br>"; // Lo primero que veremos
+
+		echo "este es el segundo mensaje <br>" // Lo segundo que veremos
+
+
+			/*
+				NOTA:
+					- En principio parece obvio pero es importante tenerlo presente, sobretodo cuando entran en juego estructuras de control que interrumpen el flujo del programa. (Condicionales, bucles, funciones)
+			*/
+
+	?>
+</body>
+</html>
+
