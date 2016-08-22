53a9fa7d20e3f3b7b1b3e6750f0ce37bda20fb3e
Antonio Llamas <antoniollamasln@gmail.com>
2016-08-22 05:38:30 +0200

    LESSON 2 was added.

---
+++ basic/lesson2.comments.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>LESSON 2: Comments</title>
+</head>
+<body>
+	<?php
+	
+		print "Welcome to php course. <br>"; // This's a inline comment
+		// echo "¡Hello World! <br>";
+		print "Until next lesson.";
+
+		/*
+			This's a multiline comment
+			
+			The comments are ignored by php interpreter, they are helpful for developers and they are often used to disable code (eg: line 11).
+
+		*/
+	?>
+</body>
+</html>
+
