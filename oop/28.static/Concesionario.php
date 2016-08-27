62156b0dd13216587fd91023ca6b183ee2f400df
Antonio Llamas <antoniollamasln@gmail.com>
2016-08-27 08:50:50 +0200

    fix v28

--- oop/28.static/Concesionario.php
+++ oop/28.static/Concesionario.php
@@ -4,6 +4,8 @@
 		
 		private $precio_base;
 		
+
+		// Descuento-> este descuento no depende del objeto es aplicable a todos los objetos. Por eso lo hacemos estatico para compartirlo a todas las instancias u objetos
 		static $ayuda=4500;
 		
 				
@@ -71,9 +73,10 @@
 		}// fin tapicería
 		
 		
-		
+		// Ahora quiero que me devuevla el precio base menos el descuento que acabo de establecer
 		function precio_final(){
-			
+
+			// Para hacer referencia a una propiedad o metodo estatico ya no podemos utilizar el $this que hace referencia a cada objeto ahora etnemos que utilizar el operador self:: (vemos que con :: se utiliza el $ para referirnos al a propiedad)
 			$valor_final=$this->precio_base-self::$ayuda;
 			
 			return $valor_final;	
--- oop/28.static/Uso_concesionario.php
+++ oop/28.static/Uso_concesionario.php
@@ -10,27 +10,37 @@
 <?php
 
 	include("Concesionario.php");
-	
+
+	// Ve esta forma vemos como esta funcionando la propiedad estatica y vemos como es comun para los dos objetos a la hora de aplicar el descuento. Pero esto vistos los proncipios de la poo no es del todo correcto ya que la propiedad ayuda no está encapsulada y desde fuera cualquiera podría cambiar su valor. Vamos a verlo con un ejemplo
+
+	// Como esta propiedad no pertenece a ningun objeto no podemos hacer referencia a ningun objeto sino a la propia clase Compra_vehiculo::$ayuda
+	// Vemos como hacerlo correctamente en la siguiente lección. 
+
 	Compra_vehiculo::$ayuda=10000;
+
 	
+	$compraAntonio = new Compra_vehiculo("compacto");
+
+	$compraAntonio->climatizador();
+
+	$compraAntonio->tapiceria_cuero("blanco");
 	
-	$compra_Antonio=new Compra_vehiculo("compacto");
+	echo $compraAntonio->precio_final() . "<br>";
 	
-	$compra_Antonio->climatizador();
-	
-	$compra_Antonio->tapiceria_cuero("blanco");
-	
-	echo $compra_Antonio->precio_final() . "<br>";
-	
-	$compra_Ana=new Compra_vehiculo("compacto");
-	
-	$compra_Ana->climatizador();
-	
-	$compra_Ana->tapiceria_cuero("rojo");
-	
-	echo $compra_Ana->precio_final();
-	
-	
+	$compraAna = new Compra_vehiculo("compacto");
+
+	$compraAna->climatizador();
+
+	$compraAna->tapiceria_cuero("rojo");
+
+	echo $compraAna->precio_final();
+
+	/*
+	NOTA: 
+
+	Cada uno de los dos objetos compraAntonio y compraAna tienen su copia de los metodos que se comportan de distinta manera en un objeto u otro. 
+	Con los métodos o variables estaticas conseguimos que ya los objetos no tenan cada uno una copia del mismo ahora pertenecen a la clase y se comportan de la misma manera para todos los objetos. 	
+	*/
 	
 	
 
